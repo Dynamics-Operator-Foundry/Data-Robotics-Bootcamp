5b322bd5a167b54dd8ad2e591edf4ffbf420e404
pattylo <19451945as@gmail.com>
2024-11-09 15:48:31 +0800

    lorentz revisit

--- a_dynamics/lorentz.py
+++ a_dynamics/lorentz.py
@@ -22,26 +22,38 @@ x0_all = []
 x1_all = []
 x2_all = []
 
+z0_all = []
+z1_all = []
+z2_all = []
+
 t = 0
 dt = 1e-3
 
 x = np.array([0.1,0,0])
+z = np.array([0.1+1e-3,0,0])
 
 while True:
     x0_all.append(x[0])
     x1_all.append(x[1])
     x2_all.append(x[2])
+    z0_all.append(z[0])
+    z1_all.append(z[1])
+    z2_all.append(z[2])
+    
     x = rk4(func=f_lorentz, x=x, h=dt)
+    z = rk4(func=f_lorentz, x=z, h=dt)
     
     t = t + dt
-    if t > 200.0:
+    if t > 100.0:
         break
 
 
 # 3D plot of the Lorenz attractor
 fig = plt.figure(figsize=(10, 6))
 ax = fig.add_subplot(111, projection='3d')
-ax.plot(x0_all, x1_all, x2_all, color='blue', lw=0.6)
+
+ax.plot(x0_all, x1_all, x2_all, color='blue', lw=0.6, alpha=0.7, label="Trajectory 1")
+ax.plot(z0_all, z1_all, z2_all, color='red', lw=0.6, alpha=0.7, label="Trajectory 2")
 
 # Labels and title
 ax.set_xlabel("x")
